8e01666fd005aa9334ddf47c157de2738bc3c247
Vijay1712 <vjagtap2@illinois.edu>
2019-03-30 19:40:55 -0500

    Snowball Fight

---
+++ UnityGame/Snowball Fight Game/Assets/Scripts/Snowball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowball : MonoBehaviour
+{
+    public float Ball_Speed;
+    private Rigidbody2D theRB;
+    public GameObject snowball_effect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Ball_Speed * transform.localScale.x, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player1")
+        {
+            FindObjectOfType<GameManager>().HurtP1();
+        }
+
+        if (other.tag == "Player2")
+        {
+            FindObjectOfType<GameManager>().HurtP2();
+        }
+        Instantiate(snowball_effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
